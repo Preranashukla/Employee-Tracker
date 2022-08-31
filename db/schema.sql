e2189ec89c86422564bb2dc28b2c541d029b82e7
Prerana Shukla <shuklaprerana01@gmail.com>
2022-08-31 12:09:28 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
 
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employee_role  (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL,
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+FOREIGN KEY (role_id)
+REFERENCES employee_role(id)
+ON DELETE CASCADE
+);
